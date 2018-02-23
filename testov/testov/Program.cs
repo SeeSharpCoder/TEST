61577da2a895746276fa8baf4cce8b6bd208e308
SeeSharpCoder <35990000+SeeSharpCoder@users.noreply.github.com>
2018-02-23 11:48:32 +0200

    Add files via upload

---
+++ testov/testov/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello github!");
+        }
+    }
+}
